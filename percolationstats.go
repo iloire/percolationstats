e077ffea02331c55dfdbf8f2f86a2a08044d9842
Ivan Loire <iloire@atlassian.com>
2015-02-09 22:17:38 +1100

    Cleanup

--- percolationstats.go
+++ percolationstats.go
@@ -11,7 +11,7 @@ import(
 
 var nFlag = flag.Int("n", 200, "size")
 var tFlag = flag.Int("t", 10, "repetitions")
-var tCPUS = flag.Int("cpus", 1, "number of CPUs")
+var tCPUSFlag = flag.Int("cpus", 1, "number of CPUs")
 
 func newRndCell(r *rand.Rand, n int) percolation.Cell {
   i:= uint(r.Intn(n))
@@ -21,10 +21,10 @@ func newRndCell(r *rand.Rand, n int) percolation.Cell {
 
 func calcPercolation(n int, c chan float32){
   r := rand.New(rand.NewSource(time.Now().UnixNano()))
-  count:=0
   p:= new(percolation.Percolation)
   p.Initialize(uint(n))
 
+  count:=0
   for !p.Percolates() {
     cell:=newRndCell(r, n)
     if !p.IsOpen(cell){
@@ -41,23 +41,19 @@ func main(){
 
   flag.Parse()
 
-  var sum float32
-  var n = *nFlag
-  var t = *tFlag
-  var cpus = *tCPUS
-
-  runtime.GOMAXPROCS(cpus)
+  runtime.GOMAXPROCS(*tCPUSFlag)
 
   c:= make(chan float32)
-  for i:=0; i<t; i++ {
-    go calcPercolation(n, c)
+  for i:=0; i<*tFlag; i++ {
+    go calcPercolation(*nFlag, c)
   }
 
-  for i:=0; i<t; i++ {
+  var sum float32
+  for i:=0; i<*tFlag; i++ {
     sum = sum + <-c
   }
   
-  fmt.Printf("\n mean: %v", sum / float32(t))
+  fmt.Printf("\n mean: %v", sum / float32(*tFlag))
   fmt.Printf("\n took: %v", time.Since(start))
 
   //TODO: stddev, confidence interval, etc.
